eb420ecd4d09738b9b558323f3d335e9c3db09e9
Hack Reactor Students <github@hackreactor.com>
2013-08-05 14:49:07 -0700

    passing basic string and array input tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,10 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+
+  if (typeof obj === 'string') {
+    return '"' + obj.toString() + '"';
+  } else if (_.isArray(obj)) {
+    return '[' + _.map(obj, stringifyJSON(obj)).join(',') + ']';
+  }
 };
